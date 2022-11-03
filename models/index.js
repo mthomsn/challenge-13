05a878ba6a6d65880cdca45dbe09bf5b1e7d781d
Matt <matthewthomsen99@gmail.com>
2022-11-02 23:56:09 -0500

    add modal associations

--- models/index.js
+++ models/index.js
@@ -3,6 +3,17 @@ const Product = require('./Product');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+Product.hasOne(Category);
 
+Category.hasMany(Product);
+
+Tag.belongsToMany(Product);
+
+Product.belongsToMany(Tag, {
+  through: {
+    modal: ProductTag,
+  },
+  as: 'product_tag',
+});
 
 module.exports = { Category, Product, Tag, ProductTag };
