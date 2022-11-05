775ce34337cda358020a6c5ab07da1aec9e1e20c
Matt <matthewthomsen99@gmail.com>
2022-11-05 00:20:32 -0500

    fix table associations

--- models/index.js
+++ models/index.js
@@ -7,20 +7,8 @@ Product.hasOne(Category);
 
 Category.hasMany(Product);
 
-Tag.belongsToMany(Product, {
-  through: {
-    modal: ProductTag,
-    // foreignKey: 'tag_id'
-  },
-  as: 'tag_rel'
-});
+Tag.belongsToMany(Product, {through: ProductTag, foreignKey: 'tag_id'});
 
-Product.belongsToMany(Tag, {
-  through: {
-    modal: ProductTag,
-    // foreignKey: 'product_id'
-  },
-  as: 'product_rel',
-});
+Product.belongsToMany(Tag, {through: ProductTag, foreignKey: 'product_id'});
 
 module.exports = { Category, Product, Tag, ProductTag };
